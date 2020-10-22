7be4edf414e02878c7b060de7373dc7a6aa3786e
yogigan <64954893+yogigan@users.noreply.github.com>
2020-10-22 22:23:39 +0700

    dynamic landing page & testimonial

---
+++ Final-Project/src/main/java/com/MII/FinalProject/entities/Testimonial.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.MII.FinalProject.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Yogi
+ */
+@Entity
+@Table(name = "testimonial")
+@NamedQueries({
+    @NamedQuery(name = "Testimonial.findAll", query = "SELECT t FROM Testimonial t")})
+public class Testimonial implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "name")
+    private String name;
+    @Basic(optional = false)
+    @Column(name = "profession")
+    private String profession;
+    @Basic(optional = false)
+    @Column(name = "subtext")
+    private String subtext;
+
+    public Testimonial() {
+    }
+
+    public Testimonial(Integer id) {
+        this.id = id;
+    }
+
+    public Testimonial(Integer id, String name, String profession, String subtext) {
+        this.id = id;
+        this.name = name;
+        this.profession = profession;
+        this.subtext = subtext;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getSubtext() {
+        return subtext;
+    }
+
+    public void setSubtext(String subtext) {
+        this.subtext = subtext;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Testimonial)) {
+            return false;
+        }
+        Testimonial other = (Testimonial) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.MII.FinalProject.entities.Testimonial[ id=" + id + " ]";
+    }
+    
+}
